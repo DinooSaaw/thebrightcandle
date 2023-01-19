8bc4e803e58f0bd753cae0d591b73073d00899ca
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-01-19 16:29:06 +1100

    added comments and event handling for the 'disconneted' event.

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-module.exports = require('./mongoose/mongoose.js');
+module.exports = require('./mongoose/mongoose.js'); // Exports the mongoose module
 
-require("dotenv").config();
+require("dotenv").config(); // Required for handling environment variables
 
-const tmi = require("tmi.js");
+const tmi = require("tmi.js"); // Required for connecting to Twitch's chat service
 
-const Bot = require("./auth/thebrightcandle.json")
-const Dino = require("./auth/dinoosaaw.json")
+const Bot = require("./auth/thebrightcandle.json") // JSON file for "BOT" client authentication
+const Dino = require("./auth/dinoosaaw.json") // JSON file for "DINO" client authentication
 
-let CLIENTS = [];
+let CLIENTS = [];  // An array to hold the clients
+ 
+class TwitchChatLib { // Class for handling Twitch events
 
-class TwitchChatLib {
-
-    async onConnectedHandler(addr, port) {
+    async onConnectedHandler(addr, port) { // Event handler for "connected" event
         console.log("Connected", addr, port);
     }
 
-
-}
-
-
-
-class BotClients {
-    async twitchChat() {
-    let tl = new TwitchChatLib();
-    CLIENTS["BOT"] = new tmi.client(Bot);
-    CLIENTS["DINO"] = new tmi.client(Dino);
-
-    CLIENTS["BOT"].on("connected", tl.onConnectedHandler);
-
-    CLIENTS["BOT"].connect();
-    CLIENTS["DINO"].connect();
+    async onDisconnectedHandler(reason) { // Event handler for "disconnected" event
+        console.log("Disconnected", reason);
     }
 }
 
-let botclients = new BotClients();
-botclients.twitchChat();
+
+
+class BotClients { // Class for creating and managing the Twitch clients
+
+    async twitchChat() {
+    let tl = new TwitchChatLib();
+    CLIENTS["BOT"] = new tmi.client(Bot);  // create new "BOT" client with the Bot authentication
+    CLIENTS["DINO"] = new tmi.client(Dino); // create new "DINO" client with the Dino authentication
+
+    CLIENTS["BOT"].on("connected", tl.onConnectedHandler); // assign the "onConnectedHandler" to the "connected" event
+
+    CLIENTS["BOT"].connect();  // connect the "BOT" client
+    CLIENTS["DINO"].connect(); // connect the "DINO" client
+    }
+}
+
+let botclients = new BotClients(); // create a new instance of BotClients
+botclients.twitchChat(); // call the twitchChat method.
