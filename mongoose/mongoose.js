5afbd13c6db9a574718e39f86d3120763f067ccb
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-01-19 17:29:21 +1100

    added comments to mongoose.js

--- mongoose/mongoose.js
+++ mongoose/mongoose.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose')
-require("dotenv").config();
+const mongoose = require('mongoose') // Require the Mongoose library
+require("dotenv").config(); // Require the Dotenv library to access environment variables
 
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DATABASEURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true, 
+mongoose.set('strictQuery', true); // Enable strict query mode in Mongoose
+mongoose.connect(process.env.DATABASEURL, { // Connect to the MongoDB database using the URL stored in the DATABASEURL environment variable
+    useNewUrlParser: true, // set options for the connection including using the new URL parser and unified topology,
+    useUnifiedTopology: true, 
+    keepAlive: true,  //  keeping the connection alive, and setting an initial delay for the keep alive option
     keepAliveInitialDelay: 300000
 });
 
+// Log a message to the console when the connection is successful
 mongoose.connection.on("connected", () => {
     console.log("Connected to Mongoose");
 });
 
+// Log a message to the console when the connection is disconnected
 mongoose.connection.on("disconnected ", () => {
     console.log("Disconnected from Mongoose");
 });
 
+// Log an error message and the error object to the console when an error occurs
 mongoose.connection.on("error", (err) => {
     console.log("Error from Mongoose", err);
 });
