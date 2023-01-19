691b89565ea9d12382d6813ae6d9c1ce9550023f
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-01-19 16:08:55 +1100

    upload main files

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const tmi = require("tmi.js");
+
+const Bot = require("./auth/thebrightcandle.json")
+const Dino = require("./auth/dinoosaaw.json")
+
+let CLIENTS = [];
+
+class TwitchChatLib {
+
+    async onConnectedHandler(addr, port) {
+        console.log("Connected", addr, port);
+    }
+
+
+}
+
+
+
+class BotClients {
+    async twitchChat() {
+    let tl = new TwitchChatLib();
+    CLIENTS["BOT"] = new tmi.client(Bot);
+    CLIENTS["DINO"] = new tmi.client(Dino);
+
+    CLIENTS["BOT"].on("connected", tl.onConnectedHandler);
+
+    CLIENTS["BOT"].connect();
+    CLIENTS["DINO"].connect();
+    }
+}
+
+let botclients = new BotClients();
+botclients.twitchChat();
