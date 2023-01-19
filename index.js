1900b4fd45f63bdc6ebeaba291b7f8b6db7aa5aa
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-01-19 20:13:23 +1100

    added bot message handler

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ require("dotenv").config(); // Required for handling environment variables
 
 const tmi = require("tmi.js"); // Required for connecting to Twitch's chat service
 const chalk = require("chalk"); // Optional for colorizing
+const { MongoClient } = require('mongodb'); // Required for connecting to MongoDB server and performing perform various operations such as CRUD
 const Bot = require("./auth/thebrightcandle.json") // JSON file for "BOT" client authentication
-const Dino = require("./auth/dinoosaaw.json") // JSON file for "DINO" client authentication
+const Dino = require("./auth/dinoosaaw.json"); // JSON file for "DINO" client authentication
 
 let CLIENTS = [];  // An array to hold the clients
- 
+
 class TwitchChatLib { // Class for handling Twitch events
 
     async onConnectedHandler(addr, port) { // Event handler for "connected" event
@@ -26,6 +27,20 @@ class TwitchChatLib { // Class for handling Twitch events
         console.log(message);
     }
 
+    async onBotMessageHandler(target, context, message, self) {
+        let msg = message.toLowerCase(); // convert the message to lowercase
+
+        const botmessageDataBase = await settingsDataBaseQuery({ _id: 'botmsgs' }) // get the array of trigger words/phrases
+        let check = botmessageDataBase.msg.filter(word => msg.includes(word)); // check if the message contains any of the trigger words/phrases
+        if(check.length > 0) {
+            console.log("onBotMessageHandler Trigged");
+
+            if (context.mod) return; // check if the sender is a moderator, if so return
+
+            CLIENTS["SELF"].ban(target, context["display-name"], `trigger phrase: ${check} | automated by TheBrightCandle`).catch((err) (console.log(err))); // ban the user who sent the message, providing the trigger phrase and the automated source
+        }
+    }
+
 }
 
 
@@ -38,10 +53,25 @@ class BotClients { // Class for creating and managing the Twitch clients
     CLIENTS["DINO"] = new tmi.client(Dino); // create new "DINO" client with the Dino authentication
 
     CLIENTS["BOT"].on("connected", tl.onConnectedHandler); // assign the "onConnectedHandler" to the "connected" event
+    CLIENTS["DINO"].on("message", tl.onBotMessageHandler); // asasing the "onBotMessageHandler" to the "message" event
 
     CLIENTS["BOT"].connect();  // connect the "BOT" client
     CLIENTS["DINO"].connect(); // connect the "DINO" client
     }
+}
+
+async function settingsDataBaseQuery(query) {
+    const MongoDBclient = new MongoClient(process.env.DATABASEURL);
+    try {
+        let database = MongoDBclient.db('twitch')
+        let settingsDataBase = database.collection('settings')
+        let result = await settingsDataBase.findOne(query);
+        // console.log(result)
+        return result
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await MongoDBclient.close();
+      }
 }
 
 let botclients = new BotClients(); // create a new instance of BotClients
